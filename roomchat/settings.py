cf6f6a4e2df22a136b1c7d774800191ecdaf11a1
udaysk3 <burluudaysantoshkumar3@gmail.com>
2023-02-08 00:29:54 +0530

    vercel changes

--- roomchat/settings.py
+++ roomchat/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9)sg^aa5#x*il!wchg*#)e+hj(8gcw7*0-tv6i+x+2wsh+c$65
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','.vercel.app','.now.sh']
 
 
 # Application definition
@@ -121,7 +121,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles_build','static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
