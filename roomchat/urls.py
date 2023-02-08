4fcb0b700890176f29c01df0735ad8d48b887571
udaysk3 <burluudaysantoshkumar3@gmail.com>
2023-02-08 19:30:03 +0530

    railway config changes made

--- roomchat/urls.py
+++ roomchat/urls.py
@@ -20,5 +20,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('chat.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
